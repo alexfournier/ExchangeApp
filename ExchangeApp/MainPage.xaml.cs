1cedb7bb8d19fb5b7f583bb997da342a5901165c
Alex Fournier <44fournier@gmail.com>
2014-03-14 02:40:07 -0400

    app commit

---
+++ ExchangeApp/MainPage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ExchangeApp.Resources;
+using System.Globalization;
+using Microsoft.Phone.Tasks;
+using System.Windows.Input;
+namespace ExchangeApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        string localString = string.Empty;
+        float exchangeRate;
+        float baseAmount;
+        float result;
+        OpenExchange exchangeObj;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            exchangeObj = new ExchangeApp.OpenExchange();
+        }
+
+        private bool GetBaseValue()
+        {
+            try
+            {
+                baseAmount = Convert.ToSingle(InputField.Text);
+                return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No Value Entered!");
+                return false;
+            }
+        }
+
+        private void CalculateResult()
+        {
+            result = baseAmount * exchangeRate;
+        }
+
+        private void DisplayResult()
+        {
+            CultureInfo cultureSelected = new CultureInfo(localString);   // create a "Culture" specifier
+            ResultField.Text = string.Format(cultureSelected, "{0:C}", result);    // convert the value to a currency, using the Euro format.
+        }
+
+        private void CADButton_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+            if (GetBaseValue())
+            {
+                exchangeRate = exchangeObj.GetExchangeRate("CAD");
+
+                if (exchangeRate > 0)
+                {
+                    localString = "en-US";
+                    //GetExchangeRate(CADButton.Name);
+                    CalculateResult();
+                    DisplayResult();
+                }
+                else
+                {
+                    MessageBox.Show("Server is unavailable. Try again");
+                }
+            }
+        }
+
+        private void EuroButton_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (GetBaseValue())
+            {
+                exchangeRate = exchangeObj.GetExchangeRate("EUR");
+                if (exchangeRate > 0)
+                {
+                    localString = "fr-FR";
+                    //GetExchangeRate(EuroButton.Name);
+                    CalculateResult();
+                    DisplayResult();
+                }
+                else
+                {
+                    MessageBox.Show("Server is unavailable. Try again");
+                }
+            }
+        }
+        private void GBPButton_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (GetBaseValue())
+            {
+                exchangeRate = exchangeObj.GetExchangeRate("GBP");
+                if (exchangeRate > 0)
+                {
+                    localString = "en-GB";
+                    //GetExchangeRate(GBPButton.Name);
+                    CalculateResult();
+                    DisplayResult();
+                }
+                else
+                {
+                    MessageBox.Show("Server is unavailable. Try again");
+                }
+            }
+        }
+        private void YenButton_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (GetBaseValue())
+            {
+                exchangeRate = exchangeObj.GetExchangeRate("JPY");
+                if (exchangeRate > 0)
+                {
+                    localString = "ja-JP";
+                    //GetExchangeRate(YenButton.Name);
+                    CalculateResult();
+                    DisplayResult();
+                }
+                else
+                {
+                    MessageBox.Show("Server is unavailable. Try again");
+                }
+            }
+        }
+
+        private void InputField_GotFocus(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            InputField.Text = "";
+        }
+
+        private void AppBarIconBtn(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AboutPage.xaml", UriKind.Relative));
+        }
+
+        private void closeKB(object sender, EventArgs e)
+        {
+            this.Focus();
+        }
+
+        private void fetchData(object sender, EventArgs e)
+        {
+            exchangeObj = new ExchangeApp.OpenExchange();
+        }
+    }
+}
---
+++ ExchangeApp/OpenExchange.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExchangeApp
+{
+    public class OpenExchange
+    {
+        public OpenExchange()
+        {
+            RetrieveExchangeRates();
+        }
+
+        private OpenExchangeResponse currencyJsonData = null;
+
+        public float GetExchangeRate(string country)
+        {
+            float val;
+            if (currencyJsonData != null)
+            {
+                try
+                {
+                    decimal rate = currencyJsonData.Rates[country]; // decimal is like float, but used for currency
+                    val = (float)rate;    // convert to float
+                }
+                catch (KeyNotFoundException)
+                {
+                    val = -1.0f;
+                }
+            }
+            else
+                val = -1.0f;    // -1 is invalid. We could check this on return.
+            return val;
+        }
+
+        [DataContract]
+        public class OpenExchangeResponse
+        {
+            [DataMember(Name = "disclaimer")]
+            public string Disclaimer { get; set; }
+
+            [DataMember(Name = "license")]
+            public string License { get; set; }
+
+            [DataMember(Name = "timestamp")]
+            public string Timestamp { get; set; }
+
+            [DataMember(Name = "base")]
+            public string Base { get; set; }
+
+            [DataMember(Name = "rates")]
+            public Dictionary<string, decimal> Rates { get; set; }
+        }
+
+        private void RetrieveExchangeRates()
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+               MessageBox.Show("Network error. Check your internet connecton!");
+            }
+            else
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+                webClient.DownloadStringAsync(new Uri("http://openexchangerates.org/api/latest.json?app_id=4873a24a1a2546f88de76f1636b640ca"));
+            }
+        }
+
+        private void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                string json = e.Result;
+                if (!string.IsNullOrEmpty(json))
+                {
+                    // parsing the Data
+                    currencyJsonData = JsonConvert.DeserializeObject<OpenExchangeResponse>(json);
+                }
+            }
+        }
+    }
+}
